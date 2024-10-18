7569ef7985ccef9443a58b225896c0b01f5e02ea
changgyu Boo <79037397+genji970@users.noreply.github.com>
2024-10-19 01:28:09 +0900

    Update build.py

--- layers/reorg/build.py
+++ layers/reorg/build.py
@@ -20,15 +20,21 @@ this_file = os.path.dirname(os.path.realpath(__file__))
 extra_objects = ['src/reorg_cuda_kernel.cu.o']
 extra_objects = [os.path.join(this_file, fname) for fname in extra_objects]
 
-ffi = CUDAExtension(
-    '_ext.reorg_layer',
-    headers=headers,
-    sources=sources,
-    define_macros=defines,
-    relative_to=__file__,
-    with_cuda=with_cuda,
-    extra_objects=extra_objects
+setup(
+    name='reorg_layer',
+    ext_modules=[
+        CUDAExtension(
+            '_ext.reorg_layer',
+            headers=['header1.h', 'header2.h'],
+            sources=['source1.cpp', 'source2.cu'],
+            define_macros=[('WITH_CUDA', None)],
+            extra_objects=[]  # 추가 오브젝트가 필요한 경우 여기에 추가
+        )
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    }
 )
 
-if __name__ == '__main__':
-    ffi.build()
+if __name__ == "__main__":
+    setup.py build
